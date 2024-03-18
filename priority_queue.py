c00876aa64b540b4dfc30d76fc741d6fae12a68b
Sugu <152015418+HyunKN@users.noreply.github.com>
2024-03-18 23:58:34 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -10,12 +10,12 @@ class PriorityQueue:
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
-      item = self.heap.pop()
+      item = self.heap.pop()[1]
       self._sift_down(0)
-      return item[1]
+      return item
     elif len(self.heap) == 1:
-      item = self.heap.pop()
-      return item[1]
+      item = self.heap.pop()[1]
+      return item
     else:
       return None
 
